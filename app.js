50b03846772b1f19cfc6e209e6e41f3993b2d88f
Rees Draminski <rees.draminski@gmail.com>
2020-06-12 19:29:11 -0400

    UI update

--- app.js
+++ app.js
@@ -8,38 +8,31 @@ const DELAY = 100;
 const PAGE_SIZE = 10;
 
 // Salary data
-let data;
+let salaryData = {};
 
 let pageNum = 0;
 let directorySearches = [];
+
+fetchAll([
+    "2019_data.csv",
+    "2018_data.csv",
+    "2017_data.csv"
+])
+.then(() => {
+    // show data loaded text then hide it after 1 second
+    let loadingIndicator = document.getElementById("loadingIndicator");
+    loadingIndicator.innerHTML = "Data loaded!";
+    setTimeout(() => loadingIndicator.style.display = "none", 1000);
+
+    // show the salary explore page
+    // showExplore();
+    search("Hrabowski");
+});
 
 /**
  * Whenever the document loads, the code inside the function will be executed.
  */
 $(document).ready(function() {
-    let loadingIndicator = document.getElementById("loadingIndicator");
-
-    // Request the data from the server
-    $.ajax({
-        type: "GET",
-        url: "data.csv",
-        dataType: "text",
-        success: function(res) {
-            // Show data loaded text then hide it after 1 second
-            loadingIndicator.innerHTML = "Data loaded!";
-            setTimeout(function() {
-                loadingIndicator.style.display = "none";
-            }, 1000);
-
-            // Convert the CSV data into JSON format
-            data = Papa.parse(res);
-            data = data.data;
-
-            // show the salary explore page
-            showExplore();
-        }
-    });
-
     /**
      * Bind the search box to automatically search
      */
@@ -50,14 +43,14 @@ $(document).ready(function() {
 
         // Clear directory search timeouts
         if (directorySearches.length)
-            for (var foo of directorySearches)
+            for (let foo of directorySearches)
                 clearTimeout(foo);
 
         // Get the text that was inputted into the search bar
         let query = $(this).val();
 
         // Wait 300ms before doing anything
-        timeout = setTimeout(function() {
+        timeout = setTimeout(() => {
             // If there is something in the search bar, search
             if (query) {
                 search(query);
@@ -66,11 +59,45 @@ $(document).ready(function() {
             // If the user cleared out the search bar, clear the results
             else {
                 document.getElementById("results").innerHTML = "";
-                showExplore();
+                // showExplore();
             }
         }, 300);
     });
 });
+
+/**
+ * Search the salary data for a user's query.
+ * 
+ * @param {string} query the user's search keyword
+ */
+function search(query) {
+    let results = {};
+
+    for (let [year, yearlyData] of Object.entries(salaryData)) {
+        for (let entry of yearlyData) {
+            // Get the person's first and last name
+            let name = entry[FIRST_NAME] + " " + entry[LAST_NAME];
+
+            // Convert the employee name and query to lowercase to be case insensitive
+            name = name.toLowerCase();
+            let caseInsensitiveQuery = query.toLowerCase();
+
+            // If the name includes the search query
+            if (name.includes(caseInsensitiveQuery)) {
+                // if the name has not been seen yet in other years
+                if (!(name in results)) {
+                    results[name] = {};
+                }
+
+                // save the person's yearly salary data
+                results[name][year] = entry;
+            }
+        }
+    }
+
+    clearResults();
+    showResults(results);
+}
 
 /**
  * Show the Salary Explore page.
@@ -83,6 +110,10 @@ function showExplore() {
     data = data.sort((a, b) => {
         return b[YTD_GROSS_EARNINGS] - a[YTD_GROSS_EARNINGS];
     });
+
+    // Clear previous results from results display (if any)
+    let resultsDisplay = document.getElementById("results");
+    resultsDisplay.innerHTML = "<h4 class='mt-3'>Explore Salary Data</h4>";
 
     // show first page of results
     showResults(data.slice(0, PAGE_SIZE));
@@ -102,8 +133,142 @@ function showExplore() {
 }
 
 /**
+ * Clear the results view.
+ */
+function clearResults() {
+    let resultsDisplay = document.getElementById("results");
+    resultsDisplay.innerHTML = "";
+}
+
+/**
+ * Display a list of results to the User Inferface.
+ * @param {array} data 
+ */
+function showResults(results) {
+    let resultsDisplay = document.getElementById("results");
+
+    // if there are not results
+    if (Object.values(results).length === 0) {
+        // create a list item
+        let el = document.createElement("LI");
+        el.className = "list-group-item";
+
+        // show "No Results Found" text
+        el.innerHTML = "<h4 class='mb-0'>No Results Found</h4>";
+
+        // add the result list item to the results display
+        resultsDisplay.appendChild(el);
+
+        return;
+    }
+
+    // go through each person in the results object
+    Object.values(results).forEach(person => {
+        // create a list item
+        let el = document.createElement("LI");
+        el.className = "list-group-item";
+
+        // keep track to only show a person's details once
+        let showedPersonalDetails = false;
+
+        // go through years in reverse chronological order
+        for (let [year, entry] of Object.entries(person).reverse()) {
+            // only show a person's details once
+            if (!showedPersonalDetails) {
+                // Get the person's first name
+                let name = nameCapitalize(entry[FIRST_NAME]);
+
+                // Get the person's middle initial (if any)
+                if (entry[MIDDLE_INITIAL] && entry[MIDDLE_INITIAL] != "NA")
+                    name += " " + entry[MIDDLE_INITIAL] + ". ";
+                else
+                    name += " ";
+
+                // Get the person's last name
+                name += nameCapitalize(entry[LAST_NAME]);
+
+                // Get the person's suffix (if any)
+                if (entry[SUFFIX] && entry[SUFFIX] != "NA") {
+                    name += entry[SUFFIX];
+                }
+
+                // Add the person's name to the list item
+                el.innerHTML += "<h4>" + name + "</h4>";
+
+                // add title and department placeholders
+                // let titleP = document.createElement("p");
+                // titleP.innerHTML = "{{title}}";
+                // titleP.style.display = "none";
+                // titleP.className = "mb-0";
+
+                // el.appendChild(titleP);
+
+                // let departmentP = document.createElement("p");
+                // departmentP.innerHTML = "{{department}}";
+                // departmentP.style.display = "none";
+                // departmentP.className = "mb-0";
+
+                // el.appendChild(departmentP);
+
+                // // Convert name to search term for UMBC Directory
+                // let searchTerm = result[FIRST_NAME] + "+" + result[LAST_NAME];
+
+                // // space out the calls to UMBC directory search
+                // setTimeout(() => searchDirectory(searchTerm, el), DELAY);
+
+                showedPersonalDetails = true;
+            }
+
+            // show the year of the salary entry
+            el.innerHTML += `<p class="yearLabel">${year}</p>`;
+
+            // create data table
+            const table = document.createElement("table");
+
+            // create row for table headers
+            const headerRow = document.createElement("tr");
+            [
+                "YTD Gross Earnings",
+                "Regular Earnings",
+                "Annual Salary",
+                "Overtime Earnings",
+                "Other Earnings"
+            ]
+            .forEach(x => {
+                const th = document.createElement("th");
+                th.innerHTML = x;
+
+                headerRow.appendChild(th);
+            });
+
+            // TODO abstract row creation
+            const dataRow = document.createElement("tr");
+            [
+                "$" + addCommas(entry[YTD_GROSS_EARNINGS]),
+                "$" + addCommas(entry[REGULAR_EARNINGS]),
+                "$" + addCommas(entry[ANNUAL_SALARY]),
+                "$" + addCommas(entry[OVERTIME_EARNINGS]),
+                "$" + addCommas(entry[OTHER_EARNINGS])
+            ]
+            .forEach(x => {
+                const td = document.createElement("td");
+                td.innerHTML = x;
+
+                dataRow.appendChild(td);
+            });
+
+            table.appendChild(headerRow);
+            table.appendChild(dataRow);
+
+            el.append(table);
+        }
+
+        resultsDisplay.appendChild(el);
+    });
+}
+
+/**
  * Search the UMBC Directory through AJAX.
- * 
  * @param {string} searchTerm search query for UMBC directory search
  * @param {Node} el target element to show result text in
  */
@@ -120,17 +285,22 @@ function searchDirectory(searchTerm, el) {
             let html = document.createElement("html");
             html.innerHTML = res;
 
+
             // Get title if the directory has it
             let titleRaw = html.getElementsByClassName("title");
-            if (titleRaw.length)
+            if (titleRaw.length) {
+		        el.children[1].style.display = "block";
                 el.innerHTML = el.innerHTML.replace("{{title}}", titleRaw[0].innerHTML.trim());
+		    }
             else
                 el.innerHTML = el.innerHTML.replace("<p class=\"mb-0\">{{title}}</p>", "");
 
             // Get department if the directory has it
             let departmentRaw = html.getElementsByClassName("department");
-            if (departmentRaw.length)
+            if (departmentRaw.length) {
+				el.children[2].style.display = "";
                 el.innerHTML = el.innerHTML.replace("{{department}}", departmentRaw[0].innerHTML.trim());
+			}
             else
                 el.innerHTML = el.innerHTML.replace("<p class=\"mb-0\">{{department}}</p>", "");
 
@@ -146,65 +316,29 @@ function searchDirectory(searchTerm, el) {
 }
 
 /**
- * 
- * @param {array} data 
+ * Fetch a list of URLs.
+ * @param {String[]} urls 
  */
-function showResults(data) {
-    // Clear previous results from results display (if any)
-    let resultsDisplay = document.getElementById("results");
-    resultsDisplay.innerHTML = "<h4 class='mt-3'>Explore Salary Data</h4>";
+function fetchAll(urls) {
+    let requests = [];
 
-    for (let result of data) {
-        // Create a list item
-        let el = document.createElement("LI");
-        el.className = "list-group-item";
+    urls.forEach(url => {
+        requests.push(
+            fetch(url)
+            .then(res => res.text())
+            .then(csv => {
+                const year = url.split("_")[0];
 
-        // Get the person's first name
-        let name = nameCapitalize(result[FIRST_NAME]);
+                salaryData[year] = Papa.parse(csv).data;
+            })
+        );
+    });
 
-        // Get the person's middle initial (if any)
-        if (result[MIDDLE_INITIAL])
-            name += " " + result[MIDDLE_INITIAL] + ". ";
-        else
-            name += " ";
-
-        // Get the person's last name
-        name += nameCapitalize(result[LAST_NAME]);
-
-        // Get the person's suffix (if any)
-        if (result[SUFFIX])
-            name += result[SUFFIX]
-
-        // Add the person's name to the list item
-        el.innerHTML += "<h4>" + name + "</h4>";
-
-        // Add title and department placeholders
-        el.innerHTML += "<p class=\"mb-0\">{{title}}</p>";
-        el.innerHTML += "<p class=\"mb-0\">{{department}}</p>";
-
-        // Convert name to search term for UMBC Directory
-        let searchTerm = result[FIRST_NAME] + "+" + result[LAST_NAME];
-
-        // space out the calls to UMBC directory search
-        setTimeout(() => {
-            searchDirectory(searchTerm, el);
-        }, DELAY);
-
-        // Add salary data
-        el.innerHTML += "<p class='mb-0'><b>YTD Gross Earnings:</b> $" + addCommas(result[YTD_GROSS_EARNINGS]) + "</p>";
-        el.innerHTML += "<p class='mb-0'><b>Regular Earnings:</b> $" + addCommas(result[REGULAR_EARNINGS]) + "</p>";
-        el.innerHTML += "<p class='mb-0'><b>Annual Salary:</b> $" + addCommas(result[ANNUAL_SALARY]) + "</p>";
-        el.innerHTML += "<p class='mb-0'><b>Overtime Earnings:</b> $" + addCommas(result[OVERTIME_EARNINGS]) + "</p>";
-        el.innerHTML += "<p class='mb-0'><b>Other Earnings:</b> $" + addCommas(result[OTHER_EARNINGS]) + "</p>";
-
-        // Add the result list item to the results display
-        resultsDisplay.appendChild(el);
-    }
+    return Promise.all(requests);
 }
 
 /**
- * Proper captilization for names.
- * 
+ * Proper capitalization for names.
  * @param {string} name 
  */
 function nameCapitalize(name) {
@@ -214,137 +348,8 @@ function nameCapitalize(name) {
 /**
  * Add commas for thousands.
  * https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
- * 
- * @param {integer} amount 
+ * @param {Number} amount 
  */
 function addCommas(amount) {
     return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");;
-}
-
-/**
- * Search the salary data for a user's query.
- * 
- * @param {string} query the user's search keyword
- */
-function search(query) {
-    let results = [];
-
-    // Go through every single person in the salary data
-    for (let entry of data) {
-        // Get the person's first and last name
-        let name = entry[FIRST_NAME] + " " + entry[LAST_NAME];
-
-        // Convert the employee name and query to lowercase to be case insensitive
-        name = name.toLowerCase();
-        let caseInsensitiveQuery = query.toLowerCase();
-
-        // If the name includes the search query
-        if (name.includes(caseInsensitiveQuery)) 
-            results.push(entry);
-    }
-
-    // Clear previous results from results display (if any)
-    let resultsDisplay = document.getElementById("results");
-    resultsDisplay.innerHTML = "";
-
-    // Show No Results Found text if no results found
-    if (results.length === 0) {
-        // Create a list item
-        let el = document.createElement("LI");
-        el.className = "list-group-item";
-
-        // Show No Results Found text
-        el.innerHTML = "<h4 class='mb-0'>No Results Found</h4>";
-
-        // Add the result list item to the results display
-        resultsDisplay.appendChild(el);
-    }
-
-    // Go through every single result
-    for (let result of results) {
-        // Create a list item
-        let el = document.createElement("LI");
-        el.className = "list-group-item";
-
-        // Get the person's first name
-        let name = nameCapitalize(result[FIRST_NAME]);
-        
-        // Get the person's middle initial (if any)
-        if (result[MIDDLE_INITIAL])
-            name += " " + result[MIDDLE_INITIAL] + ". ";
-        else
-            name += " ";
-        
-        // Get the person's last name
-        name += nameCapitalize(result[LAST_NAME]);
-
-        // Get the person's suffix (if any)
-        if (result[SUFFIX])
-            name += result[SUFFIX]
-
-        // Add the person's name to the list item
-        el.innerHTML += "<h4>" + name + "</h4>";
-
-        // Add title and department placeholders
-        el.innerHTML += "<p class=\"mb-0\">{{title}}</p>";
-        el.innerHTML += "<p class=\"mb-0\">{{department}}</p>";
-
-        // Convert name to search term for UMBC Directory
-        let searchTerm = result[FIRST_NAME] + "+" + result[LAST_NAME];
-
-        // space out the calls to UMBC directory search
-        let dirSearch = setTimeout(function() {
-            // Request from UMBC Directory Search
-            $.ajax({
-                type: "POST",
-                url: "ajax/search_directory.php",
-                data: {
-                    searchTerm: searchTerm
-                },
-                success: function(res) {
-                    // Create mock HTML
-                    let html = document.createElement("html");
-                    html.innerHTML = res;
-
-                    // Get title if the directory has it
-                    let titleRaw = html.getElementsByClassName("title");
-                    if (titleRaw.length)
-                        el.innerHTML = el.innerHTML.replace("{{title}}", titleRaw[0].innerHTML.trim());
-                    else
-                        el.innerHTML = el.innerHTML.replace("<p class=\"mb-0\">{{title}}</p>", "");
-
-                    // Get department if the directory has it
-                    let departmentRaw = html.getElementsByClassName("department");
-                    if (departmentRaw.length)
-                        el.innerHTML = el.innerHTML.replace("{{department}}", departmentRaw[0].innerHTML.trim());
-                    else
-                        el.innerHTML = el.innerHTML.replace("<p class=\"mb-0\">{{department}}</p>", "");
-
-                    // Remove mock HTML
-                    html.remove();
-                },
-                error: function (xhr, ajaxOptions, thrownError) {
-                    loadingIndicator.innerHTML = "Data load failed!";
-
-                    // if loading fails, just remove the title so user doesn't know it failed
-                    el.innerHTML = el.innerHTML.replace("<p class=\"mb-0\">{{title}}</p>", "");
-                    el.innerHTML = el.innerHTML.replace("<p class=\"mb-0\">{{department}}</p>", "");
-
-                    console.error(thrownError);
-                }
-            });
-        }, DELAY);
-
-        directorySearches.push(dirSearch);
-
-        // Add salary data
-        el.innerHTML += "<p class='mb-0'><b>YTD Gross Earnings:</b> $" + addCommas(result[YTD_GROSS_EARNINGS]) + "</p>";
-        el.innerHTML += "<p class='mb-0'><b>Regular Earnings:</b> $" + addCommas(result[REGULAR_EARNINGS]) + "</p>";
-        el.innerHTML += "<p class='mb-0'><b>Annual Salary:</b> $" + addCommas(result[ANNUAL_SALARY]) + "</p>";
-        el.innerHTML += "<p class='mb-0'><b>Overtime Earnings:</b> $" + addCommas(result[OVERTIME_EARNINGS]) + "</p>";
-        el.innerHTML += "<p class='mb-0'><b>Other Earnings:</b> $" + addCommas(result[OTHER_EARNINGS]) + "</p>";
-
-        // Add the result list item to the results display
-        resultsDisplay.appendChild(el);
-    }
 }
